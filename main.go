7bfa6b2479705893513e2753712f86c627573016
Jade Dragon <djade942@gmail.com>
2025-02-01 14:46:04 -0500

    modifications

--- main.go
+++ main.go
@@ -2,18 +2,19 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
-	"io"
-	"log"
-	"net/http"
-	"os"
-	"os/exec"
-	"path/filepath"
+    "bytes"
+    "fmt"
+    "io/ioutil"
+    "log"
+    "net/http"
+    "os"
+    "os/exec"
 )
 
 func executePythonCode(code string) (string, error) {
-    tmpfile, err := os.CreateTemp("/tmp", "code-*.py")
+    log.Printf("Executing Python Code:\n%s", code)
+    
+    tmpfile, err := os.CreateTemp("", "code-*.py")
     if err != nil {
         return "", err
     }
@@ -26,13 +27,15 @@ func executePythonCode(code string) (string, error) {
         return "", err
     }
 
-    tmpDir := filepath.Dir(tmpfile.Name())
-    cmd := exec.Command("podman", "run", "--rm", "-v", fmt.Sprintf("%s:/scripts", tmpDir), "-w", "/scripts", "python:3.11", "python", filepath.Base(tmpfile.Name()))
-
+    cmd := exec.Command("python3", "./sandbox.py", tmpfile.Name())
     var out, stderr bytes.Buffer
     cmd.Stdout = &out
     cmd.Stderr = &stderr
     err = cmd.Run()
+    
+    log.Printf("Stdout:\n%s", out.String())
+    log.Printf("Stderr:\n%s", stderr.String())
+    
     if err != nil {
         return stderr.String(), err
     }
@@ -40,29 +43,30 @@ func executePythonCode(code string) (string, error) {
 }
 
 func codeHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodPost {
-		body, err := io.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, "Unable to read request body", http.StatusBadRequest)
-			return
-		}
+    if r.Method == http.MethodPost {
+        body, err := ioutil.ReadAll(r.Body)
+        if err != nil {
+            http.Error(w, "Unable to read request body", http.StatusBadRequest)
+            return
+        }
 
-		output, err := executePythonCode(string(body))
-		if err != nil {
-			http.Error(w, output, http.StatusInternalServerError)
-			return
-		}
-		fmt.Fprint(w, output)
-	} else {
-		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-	}
+        output, err := executePythonCode(string(body))
+        if err != nil {
+            fmt.Fprint(w, output) // Send only the Python output, suppressing unnecessary errors
+            return
+        }
+        fmt.Fprint(w, output)
+    } else {
+        http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+    }
 }
 
 func main() {
-	http.HandleFunc("/execute", codeHandler)
-	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/", fs)
+    http.HandleFunc("/execute", codeHandler)
+    fs := http.FileServer(http.Dir("static"))
+    http.Handle("/", fs)
 
-	log.Println("Server started at :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+    log.Println("Server started at :8080")
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
+
