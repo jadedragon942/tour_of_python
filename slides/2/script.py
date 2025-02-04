f9cfe36bb053aaec03e7cde1ec1eddade50ac814
Jade Dragon <djade942@gmail.com>
2025-02-03 19:46:16 -0500

    revising code

--- slides/2/script.py
+++ slides/2/script.py
@@ -1,7 +1,10 @@
 import random
 
 def main():
-    # random.randint(0, 9) in Python returns a random int in [0, 9].
+    # starting a line with a '#' begins a comment. notice the next line does
+    # not get executed.
+    # print("hello world!")
+    # meanwhile, random.randint(0, 9) in Python returns a random int in [0, 9].
     favorite_number = random.randint(0, 9)
     print("My favorite number is", favorite_number)
 
