a28d05024c905b108068916f9a268012a95fcdeb
Jade Dragon <djade942@gmail.com>
2025-02-03 19:26:38 -0500

    remove mention of Go

--- slides/2/script.py
+++ slides/2/script.py
@@ -1,7 +1,6 @@
 import random
 
 def main():
-    # rand.Intn(10) in Go returns a random int in [0, 10).
     # random.randint(0, 9) in Python returns a random int in [0, 9].
     favorite_number = random.randint(0, 9)
     print("My favorite number is", favorite_number)
